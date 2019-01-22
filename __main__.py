b71e2b4541789005e7a5cd84df46926d300e3cd0
John Lownie <jlownie@rogers.com>
2019-01-21 21:18:48 -0500

    Initial commit

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from imutils.video import VideoStream
+from imutils.video import FPS
 from scipy.interpolate import interp1d
 import numpy as np
 import argparse
@@ -12,18 +13,18 @@ import time
 # parse arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--roborio", nargs="?", default="roborio-5024-frc.local", help="address to the roborio")
-ap.add_argument("-l", "--lower", nargs="+", type=int, default=[90, 224, 76], help="HSV lower bounds")
-ap.add_argument("-u", "--upper", nargs="+", type=int, default=[106, 255, 255], help="HSV upper bounds")
+ap.add_argument("-l", "--lower", nargs="+", type=int, default=[44, 0, 210], help="HSV lower bounds")
+ap.add_argument("-u", "--upper", nargs="+", type=int, default=[104, 13, 255], help="HSV upper bounds")
 ap.add_argument("-v", "--video", help="path to the video file")
 args = vars(ap.parse_args())
 
 # check for roborio
 try:
-	# requests.get("http://" + args["roborio"] + ":1181")
-	requests.get("http://192.168.24.80")
+    # requests.get("http://" + args["roborio"] + ":1181")
+    requests.get("http://192.168.24.90")
 except:
-	print("FATAL! Roborio not found")
-	exit(1)
+    print("FATAL! Roborio not found")
+    exit(1)
 
 # set the video stream
 if args.get("video", True):
@@ -37,14 +38,15 @@ else:
         width = int(vs.stream.get(3))
         height = int(vs.stream.get(4))
 
+fps = FPS().start()
+
 # allow the camera or video file to load
 time.sleep(2.0)
 
 # set some variables
-left   = interp1d([1, width / 2], [-1, 0])
-right  = interp1d([width / 2 + 1, width], [0, 1])
 lower = tuple(args["lower"]) # lower bounds of retro-reflective tape
 upper = tuple(args["upper"]) # upper bounds of retro-reflective tape
+centerX = width //2
 
 # keep looping
 while True:
@@ -62,34 +64,86 @@ while True:
     resized = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(resized, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    ratio = frame.shape[0] / float(resized.shape[0])
+    # ratio = frame.shape[0] / float(resized.shape[0])
 
     # construct a mask for the color "green", then erode and dilate
-    mask = cv2.inRange(hsv, (20, 60, 50), (37, 250, 250))
+    mask = cv2.inRange(hsv, lower, upper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
     # find contours in the mask and initialize the current (x, y) center of the ball
-    # cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = imutils.grab_contours(cnts)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
     
-    # for c in cnts:
-    #     M = cv2.moments(c)
-    #     cX = int((M["m10"] / M["m00"]) * ratio)
-    #     cY = int((M["m01"] / M["m00"]) * ratio)
+    # find closest contours to center on each side
+    min_left = 0
+    min_right = width
+    left_contour = None
+    right_contour = None
+    
+    for c in cnts:
+        area = cv2.contourArea(c)
+        # print("Area: ", area)
+        if area > 100: # ignore any noise
+            M = cv2.moments(c)
+            cX = int((M["m10"] / M["m00"])) # * ratio)
+            cY = int((M["m01"] / M["m00"])) # * ratio)
+
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            cv2.circle(resized, (int(x), int(y)), int(radius), (255, 255, 0), 1)
+           
+            if cX > centerX:
+                if cX < min_right:
+                    min_right = cX
+                    right_contour = c
+            else:
+                if cX > min_left:
+                    min_left = cX
+                    left_contour = c
     
     #     (xg, yg, wg, hg) = cv2.boundingRect(c)
     #     if wg > 10 and hg > 10:
     #         # draw the circle and centroid on the frame then update tracking points
     #         cv2.rectangle(frame, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 2)
+    
+    # draw circle around nearest contours
+    if right_contour is not None:
+        ((rX, rY), radius) = cv2.minEnclosingCircle(right_contour)
+        cv2.circle(resized, (int(rX), int(rY)), int(radius), (0, 255, 255), 1)
+        
+    if left_contour is not None:
+        ((lX, lY), radius) = cv2.minEnclosingCircle(left_contour)
+        cv2.circle(resized, (int(lX), int(lY)), int(radius), (0, 255, 255), 1)
+        
+    # print("cX: {:.2f}, lX: {:.2f}, rX: {:.2f}".format(centerX, lX, rX))
+    distance = interp1d([lX, rX], [0, 11.5])
+    try:
+        offset = distance(centerX)
+    except ValueError:
+        print("Error")
+        pass
+        
+    direction = "left" if 5.75 - offset > 0 else "right" if 5.75 - offset < 0 else "center"
+    print("The robot is {:.2f} inches to the {}.".format(abs(5.75 - offset), direction))
 
+    # draw center line
+    cv2.line(resized, (centerX, 0), (centerX, height), (255, 255, 255), 1)
+    
     # show the frame to our screen and increment the frame counter
-    cv2.imshow("Frame:", mask)
+    cv2.imshow("Frame:", resized)
+    
+    # update the FPS counter
+    fps.update()
 
     # if the 'q' key is pressed, stop the loop
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] FPS: {:.2f}".format(fps.fps()))
 
 # close the video stream
 if not args.get("video", False):
